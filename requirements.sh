7a19e0ea7ce507843d4b4a62ccfad4048a6d632c
Ivan Rudnev <isrudnev@avito.ru>
2025-03-01 22:32:03 +0300

    requirements modified

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,7 @@
+# curl -sS https://bootstrap.pypa.io/get-pip.py | python3
 pip install --upgrade pip
 pip install --upgrade --ignore-installed setuptools  #fix https://github.com/tensorflow/tensorflow/issues/622
 pip install --upgrade scikit-learn tqdm nltk editdistance joblib graphviz pandas gensim matplotlib bokeh optuna
 pip install --upgrade tensorflow
 pip install --upgrade keras
-pip install --upgrade torch
+pip install --upgrade torch tf-keras accelerate transformers
